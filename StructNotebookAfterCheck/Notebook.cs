4d9158a59e5fe4af8b28d0b9546b2e33299a205d
Vladimir Zotov <zott14@outlook.com>
2018-05-31 12:11:08 +0300

    Add StuctNotebookAfterCheck

---
+++ StructNotebookAfterCheck/Notebook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructNotebookAfterCheck
+{
+    public struct Notebook
+    {
+        readonly string model;
+        readonly string producer;
+        readonly double price;
+
+        public string Model
+        {
+            get => model;
+        }
+
+        public string Producer
+        {
+            get => producer;
+        }
+
+        public double Price
+        {
+            get => price;
+        }
+
+        public Notebook(string model, string producer, double price)
+        {
+            this.model = model;
+            this.producer = producer;
+            this.price = Math.Abs(price);
+        }
+
+        // Перегрузка пользовательского конструктора.
+
+        public Notebook(string model)
+        : this(model, "", 0)
+        {
+        }
+
+        public Notebook(double price)
+        :this("", "", price)
+        {
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Модель {0} от {1} стоимостью {2} $", model, producer, price);
+        }
+
+    }
+}
---
+++ StructNotebookAfterCheck/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructNotebookAfterCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook1 = new Notebook();
+            notebook1.Show();
+
+            Notebook notebook2 = new Notebook("TT-45", "DELL", 589.99);
+            notebook2.Show();
+
+            Notebook notebook3 = new Notebook("RR-34");
+            notebook3.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
