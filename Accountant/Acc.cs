1327c12c0eec31fcc1d03a875b7c5541a19e565d
Vladimir Zotov <zott14@outlook.com>
2018-06-26 18:40:00 +0300

    Add Accountant

---
+++ Accountant/Acc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountant
+{
+    enum Post
+    {
+        Manager = 195,
+        Developer = 200,
+        Accountant = 192,
+        Secretary = 192,
+        Cleaner =  192
+    }
+
+    class Acc
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if ((int)worker < hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Accountant/Program.cs
@@ -0,0 +1,30 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество отработанных часов: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Acc a = new Acc();
+
+            if (a.AskForBonus(Post.Developer, hours))
+            {
+                Console.WriteLine("Дать премию.");
+            }
+            else
+            {
+                Console.WriteLine("Не давать премию.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
